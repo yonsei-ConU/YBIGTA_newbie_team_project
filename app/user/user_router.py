de48248e8423564273b84388ba07de77710d3782
yonsei-ConU <53137643+yonsei-ConU@users.noreply.github.com>
2025-07-17 09:58:12 +0900

    feat: implement user_router.py

--- app/user/user_router.py
+++ app/user/user_router.py
@@ -18,17 +18,26 @@ def login_user(user_login: UserLogin, service: UserService = Depends(get_user_se
 
 @user.post("/register", response_model=BaseResponse[User], status_code=status.HTTP_201_CREATED)
 def register_user(user: User, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
-    ## TODO
-    return None
+    try:
+        user = service.register_user(user)
+        return BaseResponse(status="success", data=user, message="User Registragion Success.")
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 
 @user.delete("/delete", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
 def delete_user(user_delete_request: UserDeleteRequest, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
-    ## TODO
-    return None
+    try:
+        deleted = service.delete_user(user_delete_request)
+        return BaseResponse(status="success", data=deleted, message="User Deletion Success.")
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail="User Not Found.")
 
 
 @user.put("/update-password", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
 def update_user_password(user_update: UserUpdate, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
-    ## TODO
-    return None
+    try:
+        user = service.update_user_pwd(user_update)
+        return BaseResponse(status="success", data=user, message="User Password Update Success.")
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail="User Not Found.")
