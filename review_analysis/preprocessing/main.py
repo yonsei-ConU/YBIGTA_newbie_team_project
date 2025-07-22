4b104c8719509bf511a5287c0b9b47efddc2502d
nohyoobin <binnynoh@gmail.com>
2025-07-23 04:07:28 +0900

    feat: add Rotten + Naver 전처리기 병합 및 작동 확인

--- review_analysis/preprocessing/main.py
+++ review_analysis/preprocessing/main.py
@@ -4,16 +4,17 @@ from argparse import ArgumentParser
 from typing import Dict, Type
 from review_analysis.preprocessing.base_processor import BaseDataProcessor
 from review_analysis.preprocessing.naver_preprocessor import NaverPreprocessor
+from review_analysis.preprocessing.rotten_processor import RottenTomatoesProcessor
 
 # 모든 preprocessing 클래스를 예시 형식으로 적어주세요. 
 # key는 "reviews_사이트이름"으로, value는 해당 처리를 위한 클래스
-
-PREPROCESS_CLASSES = {
-    "reviews_naver": NaverPreprocessor,
+PREPROCESS_CLASSES: Dict[str, Type[BaseDataProcessor]] = {
+    "reviews_rotten": RottenTomatoesProcessor,
+    "reviews_naver": NaverPreprocessor
+    # 필요 시 다른 processor도 여기에 추가
 }
 
-    # key는 크롤링한 csv파일 이름으로 적어주세요! ex. reviews_naver.csv -> reviews_naver
-
+# key는 크롤링한 csv파일 이름으로 적어주세요! ex. reviews_naver.csv -> reviews_naver
 REVIEW_COLLECTIONS = glob.glob(os.path.join("database", "reviews_*.csv"))
 
 def create_parser() -> ArgumentParser:
