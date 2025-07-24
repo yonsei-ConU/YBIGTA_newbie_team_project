8dede26f164223c28eedb07b1915756600e5e7be
yonsei-ConU <53137643+yonsei-ConU@users.noreply.github.com>
2025-07-24 12:11:58 +0900

    feat: add Letterboxd preprocessor and update preprocessing classes

--- review_analysis/preprocessing/main.py
+++ review_analysis/preprocessing/main.py
@@ -5,12 +5,14 @@ from typing import Dict, Type
 from review_analysis.preprocessing.base_processor import BaseDataProcessor
 from review_analysis.preprocessing.naver_preprocessor import NaverPreprocessor
 from review_analysis.preprocessing.rotten_processor import RottenTomatoesProcessor
+from review_analysis.preprocessing.letterboxd_preprocessor import LetterboxdPreprocessor
 
 # 모든 preprocessing 클래스를 예시 형식으로 적어주세요. 
 # key는 "reviews_사이트이름"으로, value는 해당 처리를 위한 클래스
 PREPROCESS_CLASSES: Dict[str, Type[BaseDataProcessor]] = {
     "reviews_rotten": RottenTomatoesProcessor,
-    "reviews_naver": NaverPreprocessor
+    "reviews_naver": NaverPreprocessor,
+    "reviews_letterboxd": LetterboxdPreprocessor
     # 필요 시 다른 processor도 여기에 추가
 }
 
