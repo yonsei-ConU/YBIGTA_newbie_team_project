823a6684f8822e87a6b77ffe8da47625385d7f2e
nohyoobin <binnynoh@gmail.com>
2025-08-04 02:34:56 +0900

    Dockerhub에 이미지 올리기

--- app/dependencies.py
+++ app/dependencies.py
@@ -4,7 +4,7 @@ from fastapi import Depends
 
 from database.mysql_connection import SessionLocal
 from app.user.user_repository import UserRepository
-from app.user.user_repository import UserService
+from app.user.user_service import UserService  # ✅ 수정된 import
 
 
 def get_db() -> Generator[Session, None, None]:
@@ -24,3 +24,10 @@ def get_user_repository(db: Session = Depends(get_db)) -> UserRepository:
     FastAPI에서 사용할 UserRepository 객체를 의존성 주입으로 제공
     """
     return UserRepository(db)
+
+
+def get_user_service(db: Session = Depends(get_db)) -> UserService:
+    """
+    FastAPI에서 사용할 UserService 객체를 의존성 주입으로 제공
+    """
+    return UserService(db)
