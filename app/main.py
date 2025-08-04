b2a0b05b8d43096d249e166fe760714b437d2f8b
YoungWn <myu3271@gmail.com>
2025-08-04 12:40:59 +0900

    mongodb update

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import uvicorn
 import os
 
 from app.user.user_router import user
+from app.review.review_router import router as review_router
 from app.config import PORT
 
 app = FastAPI()
@@ -11,6 +12,7 @@ static_path = os.path.join(os.path.dirname(__file__), "static")
 app.mount("/static", StaticFiles(directory=static_path), name="static")
 
 app.include_router(user)
+app.include_router(review_router, prefix="/review")
 
 if __name__=="__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=PORT, reload=True)
